dbcc90f06eb42478dc2333355883c31ddeb7f573
aaron <aarongrycner@gmail.com>
2024-03-01 11:43:13 -0700

    Added test

---
+++ test/tests.cpp
@@ -0,0 +1,16 @@
+//
+// Created by aaron on 3/1/24.
+//
+
+bool messengerTest() {
+    // MasterMessenger masterMessenger;
+    // ApprenticeMessenger apprenticeMessenger;
+    // masterMessenger.hello(0);
+    // masterMessenger.goodbye();
+    // masterMessenger.sendWork(0, {"work"});
+    // masterMessenger.receiveResults();
+    // apprenticeMessenger.sendWorkResult(true);
+    // apprenticeMessenger.acknowledge();
+    // apprenticeMessenger.sendResultFound();
+    return true;
+}
